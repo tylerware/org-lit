287096e75a76733fe67a0b14d75d0f280c0c1253
Tyler Ware <tyler.ware.777@gmail.com>
2021-01-21 10:22:48 -0700

    Repo for my literate libraries
    Currently I have some modifications to org-mode that
    enable the ability to manage many org files together.
    
    I don't like the 'one org file to rule them all' notion
    that I typically see from literate projects / examples,
    which is some of the motivation for why I made it. I would
    like the generalize what I've developed to extend to any project.

---
+++ org-lit.el
@@ -0,0 +1,24 @@
+;;; org-lit.el --- manage org based literate programming projects -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Tyler Ware
+;;
+;; Author: Tyler Ware <https://github.com/tylerware>
+;; Created: January 21, 2021
+;; Modified: January 21, 2021
+;; Version: 0.0.1
+;; Keywords: org-mode, literate-programming
+;; Homepage: https://github.com/tylerware/org-lit
+;; Package-Requires: ((emacs 28.0.50) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Manage org based literate programming projects.
+;;
+;;; Code:
+
+
+
+(provide 'org-lit)
+;;; org-lit.el ends here
